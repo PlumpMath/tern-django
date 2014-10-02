1db6932a1866ebf8447cafa0ef7f3faab1ec997b
Malyshev Artem <proofit404@gmail.com>
2014-10-02 10:43:24 +0400

    Add package header.

---
+++ tern-django.el
@@ -0,0 +1,29 @@
+;;; tern-django.el --- Create tern projects for django applications.
+
+;; Copyright (C) 2014 by Malyshev Artem
+
+;; Author: Malyshev Artem <proofit404@gmail.com>
+;; URL: https://github.com/proofit404/tern-django
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24") (tern "0.0.1"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Code:
+
+(provide 'tern-django)
+
+;;; tern-django.el ends here
