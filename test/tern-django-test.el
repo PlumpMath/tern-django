e75df22d1fac588380de3a66b0e4a646ec7bb6d3
Malyshev Artem <proofit404@gmail.com>
2014-10-10 22:41:46 +0400

    Setup test suite.

---
+++ test/tern-django-test.el
@@ -0,0 +1,12 @@
+;;; tern-django-test.el --- tern-django test suite
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'ert)
+(require 'tern-django)
+
+(provide 'tern-django-test)
+
+;;; tern-django-test.el ends here
