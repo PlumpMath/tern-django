ee5fd9b12b5036bc9948a1243c911e8d0b8fd76b
Malyshev Artem <proofit404@gmail.com>
2014-11-06 23:38:02 +0300

    Make applications compatible with older django versions.

--- tern_django.py
+++ tern_django.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-from django.apps import apps
 import django
+import os
+
+django_version = django.get_version()
 
 tern_file = '.tern_project'
 tern_project = {"loadEagerly": ["static/**/*.js"]}
@@ -10,5 +12,18 @@ tern_project = {"loadEagerly": ["static/**/*.js"]}
 def applications():
     """Collect directories with django applications."""
 
-    django.setup()
-    return [app.path for app in apps.get_app_configs()]
+    if django_version >= '1.7':
+        from django.apps import apps
+        django.setup()
+        return [app.path for app in apps.get_app_configs()]
+    else:
+        from django.conf import settings
+        from django.utils.importlib import import_module
+        from django.utils._os import upath
+        directories = []
+        apps = settings.INSTALLED_APPS
+        for app in apps:
+            mod = import_module(app)
+            mod_path = os.path.dirname(upath(mod.__file__))
+            directories.append(mod_path)
+        return directories
