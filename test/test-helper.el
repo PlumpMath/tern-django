19673cd5b963a1dbe52a090d15744735c851a9e2
Malyshev Artem <proofit404@gmail.com>
2014-12-05 12:41:59 +0300

    Setup test coverage measure.

--- test/test-helper.el
+++ test/test-helper.el
@@ -6,6 +6,9 @@
 
 (require 'cask)
 
+(require 'undercover)
+(undercover "tern-django.el" (:report-file "emacs-coveralls.json"))
+
 (let ((root-directory (locate-dominating-file load-file-name "Cask")))
   (cask-initialize root-directory)
   (add-to-list 'load-path root-directory))
