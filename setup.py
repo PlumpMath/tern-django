3f1a7032611b2ba822e87728a16e78fddecee371
Malyshev Artem <proofit404@gmail.com>
2014-12-07 22:39:34 +0300

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name='tern_django',
+      version='0.1',
+      description='Create Tern projects for Django applications.',
+      url='https://github.com/proofit404/tern-django')
