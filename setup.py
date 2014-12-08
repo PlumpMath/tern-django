1670799990d498686a87ae391bd3e4d0c66b2fb7
Malyshev Artem <proofit404@gmail.com>
2014-12-08 11:12:55 +0300

    Improve setup.py with package information.

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
 from setuptools import setup
 
+readme = open('README.rst').read()
+
 setup(name='tern_django',
-      version='0.1',
+      version='0.1.0',
+      url='https://github.com/proofit404/tern-django',
       description='Create Tern projects for Django applications.',
-      url='https://github.com/proofit404/tern-django')
+      long_description=readme,
+      platforms='any',
+      license='GPL3',
+      author='Malyshev Artem',
+      author_email='proofit404@gmail.com',
+      maintainer='Malyshev Artem',
+      maintainer_email='proofit404@gmail.com',
+      py_modules=['tern_django'],
+      entry_points={
+          'console_scripts': [
+              'tern_django=tern_django:run_tern_django',
+          ],
+      },
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: JavaScript',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Text Editors',
+          'Topic :: Text Editors :: Emacs',
+      ])
