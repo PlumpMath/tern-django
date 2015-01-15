f815d761fbc3b1df454c3f34d861b5a924a457dd
Malyshev Artem <proofit404@gmail.com>
2015-01-15 21:59:24 +0300

    Update copyright years.

--- tern-django.el
+++ tern-django.el
@@ -1,6 +1,6 @@
 ;;; tern-django.el --- Create tern projects for django applications.
 
-;; Copyright (C) 2014 by Malyshev Artem
+;; Copyright (C) 2014-2015 by Malyshev Artem
 
 ;; Author: Malyshev Artem <proofit404@gmail.com>
 ;; URL: https://github.com/proofit404/tern-django
